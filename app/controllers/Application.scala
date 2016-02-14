ea83035ff25c3fe5ede8610eed8e26b08d043121
BriacPi <briacpiriou@orange.fr>
2016-02-14 21:20:11 +0100

    Fix: services

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -23,20 +23,6 @@ class Application @Inject() (val env: AuthenticationEnvironment, val messagesApi
   def myProfile = SecuredAction.async { implicit request =>
     Future.successful(Ok(views.html.myProfile()))
   }
-  // REQUIRED ROLES: serviceA (or master)
-  def serviceA = SecuredAction(WithService("serviceA")).async { implicit request =>
-    Future.successful(Ok(views.html.serviceA()))
-  }
-
-  // REQUIRED ROLES: serviceA OR serviceB (or master)
-  def serviceAorServiceB = SecuredAction(WithService("serviceA", "serviceB")).async { implicit request =>
-    Future.successful(Ok(views.html.serviceAorServiceB()))
-  }
-
-  // REQUIRED ROLES: serviceA AND serviceB (or master)
-  def serviceAandServiceB = SecuredAction(WithServices("serviceA", "serviceB")).async { implicit request =>
-    Future.successful(Ok(views.html.serviceAandServiceB()))
-  }
 
   // REQUIRED ROLES: master
   def settings = SecuredAction(WithService("master")).async { implicit request =>
