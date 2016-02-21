3fd9c812b38213ee148424fe2f2aa0be8cec2974
BriacPi <briacpiriou@orange.fr>
2016-02-21 20:33:06 +0100

    fix: likes

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,27 +1,28 @@
 $(function () {
 
-    $("#like-auth").click(function (event) {
-        var requestLikeUnlike=jsRoutes.controllers.ContentController.likeUnlike($("#post").attr("articleid"));
-        requestLikeUnlike.ajax({
-            success: function(result) {
-            },
-            failure: function(err) {
+    $(".like-auth").each(function(like){
+        $(this).click(function (event) {
+            var requestLikeUnlike=jsRoutes.controllers.ContentController.likeUnlike($(this).attr("articleid"));
+            requestLikeUnlike.ajax({
+                success: function(result) {
+                },
+                failure: function(err) {
+                }
+            });
+
+            if ($(this).find($(".fa")).hasClass('fa-heart')) {
+                $(this).find($(".fa")).removeClass('fa-heart').addClass('fa-heart-o');
+                $(this).next("#nblikes").html(parseInt($(this).next("#nblikes").html(), 10)-1);
             }
+            else if ($(this).find($(".fa")).hasClass('fa-heart-o')) {
+                $(this).find($(".fa")).removeClass('fa-heart-o').addClass('fa-heart');
+                $(this).next("#nblikes").html(parseInt($(this).next("#nblikes").html(), 10)+1);
+            }
+
+
+
         });
-
-        if ($(this).find($(".fa")).hasClass('fa-heart')) {
-            $(this).find($(".fa")).removeClass('fa-heart').addClass('fa-heart-o');
-            $(this).next().slideDown(200);
-            $("#nblikes").html(parseInt($("#nblikes").html(), 10)-1);
-        }
-        else if ($(this).find($(".fa")).hasClass('fa-heart-o')) {
-            $(this).find($(".fa")).removeClass('fa-heart-o').addClass('fa-heart');
-            $("#navi ul li div").slideUp(200);
-            $("#nblikes").html(parseInt($("#nblikes").html(), 10)+1);
-        }
-
-
-
     });
+
 });
 
