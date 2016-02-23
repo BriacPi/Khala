b9275947f74dbd0f0653fb54f94380f21f9a2f81
BriacPi <briacpiriou@orange.fr>
2016-02-23 16:35:58 +0100

    add: article reader

---
+++ app/assets/javascripts/write.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("button").click(function(event){
+        var content=contentEditor.serialize()["element-0"].value;
+        var title= titleEditor.serialize()["element-0"].value.replace(/<(?:.|\n)*?>/gm, '');
+        console.log(content);
+        $.post("demo_test_post.asp",
+            {
+                name: "Donald Duck",
+                city: "Duckburg"
+            },
+            function(data, status){
+                alert("Data: " + data + "\nStatus: " + status);
+            });
+    });
+});
