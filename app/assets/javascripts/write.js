2882403fdc1d8be2e4ae2a53e5218c0cc3eb8dac
Su YANG <ys19901010@hotmail.fr>
2016-04-27 23:22:31 +0200

    adding popover when blank content and title

--- app/assets/javascripts/write.js
+++ app/assets/javascripts/write.js
@@ -1,66 +1,47 @@
 $(document).ready(function () {
 
-    $('[data-toggle="popover"]').popover();
-
     //$("#publishButton").hide();
 
-    $("#publishButton").click(function (event) {
+    $("#publishButton").popover({trigger: 'manual'}).click(function (event) {
+
         publish();
+
     });
 
-
-    //$("#title-editable").keyup(function () {
-    //    if ($(this).val()) {
-    //        $("#publishButton").show();
-    //    }
-    //    else {
-    //        //if ($("#content-editable").val()) {
-    //        //    $("#publishButton").show();
-    //        //}
-    //        //else {
-    //        //    $("#publishButton").hide();
-    //        //}
-    //    }
-    //});
-
-    //$("#content-editable").keyup(function () {
-    //    if ($(this).val()) {
-    //        $("#publishButton").show();
-    //    }
-    //    else {
-    //        if ($("#title-editable").val()) {
-    //            $("#publishButton").show();
-    //        }
-    //        else {
-    //            $("#publishButton").hide();
-    //        }
-    //    }
-    //});
 });
 
 function publish() {
     var content = contentEditor.serialize()["element-0"].value;
     var title = titleEditor.serialize()["element-0"].value.replace(/<(?:.|\n)*?>/gm, '');
     var summary = "";
-    var arr = {
-        _id: id,
-        title: title,
-        summary: summary,
-        content: content
-        //tag1: tag1,
-        //tag2: tag2
-    };
-    $.ajax({
-        url: '/api/publish/public',
-        type: 'POST',
-        data: JSON.stringify(arr),
-        contentType: 'application/json; charset=utf-8',
-        dataType: 'json',
-        async: false,
-        success: function (msg) {
-            window.location.href = '/article/id/' + id.toString();
-        }
-    });
+    if (title && content) {
+        //alert("I am an alert box 1!");
+        var arr = {
+            _id: id,
+            title: title,
+            summary: summary,
+            content: content
+            //tag1: tag1,
+            //tag2: tag2
+        };
+        $.ajax({
+            url: '/api/publish/public',
+            type: 'POST',
+            data: JSON.stringify(arr),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            async: false,
+            success: function (msg) {
+                window.location.href = '/article/id/' + id.toString();
+            }
+        });
+    }
+    else {
+        //alert("No empty content can be published!");
+        $("[data-toggle='popover']").popover('show');
+        //$('[data-toggle="popover"]').popover("hide", {animation:true ,delay: {show: 500, hide: 1500}});
+
+    }
 
 }
 
@@ -73,9 +54,9 @@ function saveEditableContent(tag1, tag2) {
         _id: id,
         title: title,
         summary: summary,
-        content: content,
-        tag1: tag1,
-        tag2: tag2
+        content: content
+        //tag1: tag1,
+        //tag2: tag2
     };
     $.ajax({
         url: '/api/save',
